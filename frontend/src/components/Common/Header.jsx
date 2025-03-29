cdb240ef32dcadf4e3f167383e44752191661544
Pate0842 <thaihoangan1011@gmail.com>
2025-03-29 13:52:37 +0700

    initial commit

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,18 @@
+import Topbar from "../Layout/Topbar";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="border-b border-gray-200">
+        {/* Topbar */}
+        <Topbar />
+        {/* Navbar */}
+        <Navbar />
+        {/* Cart Drawer */}
+      </header>
+    </div>
+  );
+};
+
+export default Header;
